3fdc911a043ff862e01a375bf7f837843b0238cb
RoryHarrison <roryharrison5@hotmail.co.uk>
2022-10-19 00:32:57 +0100

    Gradient based on ray Y

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,41 @@ mod color;
 mod ray;
 use crate::vec3::*;
 use crate::color::write_color;
+use crate::ray::Ray;
 
 fn main(){
 
     //Image
-    const IMAGE_HEIGHT: u32 = 256;
-    const IMAGE_WIDTH: u32 = 256;
+    const ASPECT_RATIO: f32 = 16.0/9.0;
+    const IMAGE_WIDTH: u32 = 400;
+    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f32/ASPECT_RATIO) as u32;
+
+    //Camera
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0,0.0,0.0);
+    let horizontal = Vec3::new(viewport_width,0.0,0.0);
+    let vertical = Vec3::new(0.0,viewport_height,0.0);
+    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3::new(0.0,0.0,focal_length);
     
+    //Render
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    //Render
     for j in (0..IMAGE_HEIGHT).rev() {
         for i in 0..IMAGE_WIDTH {
-            let pixel_color: Color = Color::new(i as f32/(IMAGE_WIDTH-1) as f32, j as f32/(IMAGE_HEIGHT-1) as f32, 0.25);
+            let u = i as f32/(IMAGE_WIDTH as f32-1.0);
+            let v = j as f32/(IMAGE_HEIGHT as f32-1.0);
+            let r = Ray::new(origin, lower_left_corner + horizontal*u + vertical*v - origin);
+            let pixel_color: Color = ray_color(r);
             write_color(pixel_color);
         }
     }
+}
+
+fn ray_color(r: Ray) -> Color {
+    let unit_direction: Vec3 = unit_vector(r.direction);
+    let t = 0.5*(unit_direction.y + 1.0);
+    Color::new(1.0, 1.0, 1.0)*(1.0-t) + Color::new(0.5, 0.7, 1.0)*t
 }
--- src/ray.rs
+++ src/ray.rs
@@ -1,11 +1,18 @@
-use crate::vec3::*;
+use crate::vec3::{Vec3, Point3};
 
 pub struct Ray{
-    origin: Point3,
-    direction: Vec3,
+    pub origin: Point3,
+    pub direction: Vec3,
 }
 
 impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Ray {
+        Ray {
+            origin,
+            direction,
+        }
+    }
+
     pub fn at(&self, t: f32) -> Point3 {
         self.origin + self.direction*t
     }
