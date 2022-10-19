be0c9ceda627fa3651a3efc65cb613adcef70779
RoryHarrison <roryharrison5@hotmail.co.uk>
2022-10-19 12:08:54 +0100

    Adding shading based on surface normals

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,26 @@ fn main(){
 }
 
 fn ray_color(r: Ray) -> Color {
+    let t = hit_sphere(Point3::new(0.0,0.0,-1.0), 0.5, &r);
+    if t>0.0 {
+        let n = unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return Color::new(n.x+1.0, n.y+1.0, n.z+1.0) * 0.5
+    }
     let unit_direction: Vec3 = unit_vector(r.direction);
     let t = 0.5*(unit_direction.y + 1.0);
     Color::new(1.0, 1.0, 1.0)*(1.0-t) + Color::new(0.5, 0.7, 1.0)*t
+}
+
+fn hit_sphere(center: Point3, radius: f32, r: &Ray) -> f32 {
+    let oc = r.origin - center;
+    let a = r.direction.len_squared();
+    let half_b = dot(&oc, &r.direction);
+    let c = oc.len_squared() - radius*radius;
+    let discriminant = half_b*half_b - a*c;
+
+    if discriminant < 0.0 {
+        -1.0
+    }else{
+        (-half_b -(discriminant.sqrt())) / a
+    }
 }
